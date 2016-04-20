90f5e3d2e28b7db3af4171dcb8f846514487c6f1
muji <freeformsystems@gmail.com>
2016-04-20 12:55:27 +0800

    Parse child selectors.

---
+++ test/spec/compiler/child-selector.js
@@ -0,0 +1,29 @@
+var expect = require('chai').expect
+  , ast = require('mkast')
+  , Node = ast.Node
+  , compile = require('../../../lib/compiler');
+
+describe('compiler:', function() {
+  
+  it('should compile child selectors (whitespace)', function(done) {
+    var selector = 'p em'
+      , result = compile(selector);
+
+    expect(result).to.be.an('object');
+    expect(result.selectors).to.be.an('array')
+      .to.have.length(1);
+    var selected = result.selectors[0];
+    expect(selected.tag).to.eql('p');
+    expect(selected.type).to.eql(Node.PARAGRAPH);
+
+    expect(selected.selectors).to.be.an('array')
+      .to.have.length(1);
+
+    var child = selected.selectors[0];
+    expect(child.tag).to.eql('em');
+    expect(child.type).to.eql(Node.EMPH);
+
+    done();
+  });
+
+});
