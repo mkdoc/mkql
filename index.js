5a2190f6ddddab6fa2208731ecc5b195a2841aa8
muji <freeformsystems@gmail.com>
2016-04-20 13:40:21 +0800

    Sketch selector methods.

--- index.js
+++ index.js
@@ -11,6 +11,56 @@ function compile(source) {
   return compiler(source);
 }
 
+/**
+ *  Query a markdown document tree with a source selector.
+ *
+ *  If the markdown parameter is a string it is parsed into a document tree.
+ *
+ *  If the given source selector is a string it is compiled otherwise it should 
+ *  be a previously compiled result tree.
+ *
+ *  @function query
+ *  @param {Array|Object|String} markdown input data.
+ *  @param {String} source input selector.
+ *  
+ *  @returns Array list of matched nodes.
+ */
+function query(markdown, source) {
+  var ast = require('mkast')
+    , doc
+    , child
+    , i
+    , nodes = []
+    , list = [];
+
+  if(typeof source === 'string') {
+    source = compile(source); 
+  }
+
+  if(typeof markdown === 'string') {
+    doc = ast.parse(markdown);
+    child = doc.firstChild;
+    while(child) {
+      nodes.push(child); 
+      child = child.next;
+    }
+  // assume existing node object
+  }else if(markdown && !Array.isArray(markdown)) {
+    nodes = [markdown]; 
+  }
+
+  // iterate over 
+  if(source.selectors) {
+    for(i = 0;i < source.selectors.length;i++) {
+      source.selectors[i].exec(nodes, list);
+      //list = list.concat(); 
+    }
+  }
+
+  return list;
+}
+
 module.exports = {
-  compile: compile
+  compile: compile,
+  query: query
 };
