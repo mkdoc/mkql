6a09535fcaae163b5e232fe562ae6274e8c9cc4e
muji <freeformsystems@gmail.com>
2016-04-21 20:33:52 +0800

    Improve :nth-child logic.

--- test/spec/query/pseudo-nth-child.js
+++ test/spec/query/pseudo-nth-child.js
@@ -3,6 +3,11 @@ var expect = require('chai').expect
   , Node = ast.Node
   , query = require('../../../index').query;
 
+/**
+ *  @see https://developer.mozilla.org/en-US/docs/Web/CSS/:nth-child
+ *  @see https://css-tricks.com/how-nth-child-works/
+ */
+
 describe('query:', function() {
   
   it('should query with pseudo selector :nth-child(0n+1)', function(done) {
@@ -82,5 +87,68 @@ describe('query:', function() {
     }
   );
 
+  it('should query with pseudo selector :nth-child(4n+4)',
+    function(done) {
+      var selector = 'ul li:nth-child(4n+4)'
+        , result = query(
+            '* 1\n* 2\n* 3\n* 4\n* 5\n* 6\n* 7\n* 8\n\n', selector);
+      expect(result).to.be.an('array')
+        .to.have.length(2);
+      expect(result[0].type).to.eql(Node.ITEM);
+      expect(result[0].firstChild.firstChild.literal).to.eql('4');
+      expect(result[1].type).to.eql(Node.ITEM);
+      expect(result[1].firstChild.firstChild.literal).to.eql('8');
+      done();
+    }
+  );
+
+  it('should query with pseudo selector :nth-child(4n)',
+    function(done) {
+      var selector = 'ul li:nth-child(4n)'
+        , result = query(
+            '* 1\n* 2\n* 3\n* 4\n* 5\n* 6\n* 7\n* 8\n\n', selector);
+      expect(result).to.be.an('array')
+        .to.have.length(2);
+      expect(result[0].type).to.eql(Node.ITEM);
+      expect(result[0].firstChild.firstChild.literal).to.eql('4');
+      expect(result[1].type).to.eql(Node.ITEM);
+      expect(result[1].firstChild.firstChild.literal).to.eql('8');
+      done();
+    }
+  );
+
+  it('should query with pseudo selector :nth-child(5n-2)',
+    function(done) {
+      var selector = 'ul li:nth-child(5n-2)'
+        , result = query(
+            '* 1\n* 2\n* 3\n* 4\n* 5\n* 6\n* 7\n* 8\n\n', selector);
+      expect(result).to.be.an('array')
+        .to.have.length(2);
+      expect(result[0].type).to.eql(Node.ITEM);
+      expect(result[0].firstChild.firstChild.literal).to.eql('3');
+      expect(result[1].type).to.eql(Node.ITEM);
+      expect(result[1].firstChild.firstChild.literal).to.eql('8');
+      done();
+    }
+  );
+
+  it('should query with pseudo selector :nth-child(-n+3)',
+    function(done) {
+      var selector = 'ul li:nth-child(-n+3)'
+        , result = query(
+            '* 1\n* 2\n* 3\n* 4\n* 5\n* 6\n* 7\n* 8\n\n', selector);
+      //console.error(Node.serialize(result[0]))
+      //console.error(Node.serialize(result[1]))
+      expect(result).to.be.an('array')
+        .to.have.length(3);
+      expect(result[0].type).to.eql(Node.ITEM);
+      expect(result[0].firstChild.firstChild.literal).to.eql('1');
+      expect(result[1].type).to.eql(Node.ITEM);
+      expect(result[1].firstChild.firstChild.literal).to.eql('2');
+      expect(result[2].type).to.eql(Node.ITEM);
+      expect(result[2].firstChild.firstChild.literal).to.eql('3');
+      done();
+    }
+  );
 
 });
