83ca7ba844793d208754dbd5bbc4453ad97fa976
muji <freeformsystems@gmail.com>
2016-04-23 08:28:55 +0800

    Support next/adjacent sibling combinator (+).

---
+++ test/spec/query/next-sibling-combinator.js
@@ -0,0 +1,31 @@
+var expect = require('chai').expect
+  , ast = require('mkast')
+  , Node = ast.Node
+  , query = require('../../../index').query;
+
+describe('query:', function() {
+
+  it('should query with next sibling combinator (p em + text)', function(done) {
+    var selector = 'p em + text'
+      , result = query('Paragraph *emph*, **strong** and `code`', selector);
+    expect(result.length).to.eql(1);
+
+    expect(result[0].type).to.eql(Node.TEXT);
+    expect(result[0].literal).to.eql(', ');
+    done();
+  });
+
+  it('should query with next sibling combinator (p * + text)', function(done) {
+    var selector = 'p * + text'
+      , result = query('Paragraph *emph*, **strong** and `code`', selector);
+    expect(result.length).to.eql(2);
+
+    expect(result[0].type).to.eql(Node.TEXT);
+    expect(result[0].literal).to.eql(', ');
+    expect(result[1].type).to.eql(Node.TEXT);
+    expect(result[1].literal).to.eql(' and ');
+    done();
+  });
+
+
+});
