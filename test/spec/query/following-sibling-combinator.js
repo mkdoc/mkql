d5a2ccd31adc345409d6746cedde17998a4ec3f4
muji <freeformsystems@gmail.com>
2016-04-23 08:40:48 +0800

    Support following sibling combinator (~).

--- test/spec/query/following-sibling-combinator.js
+++ test/spec/query/following-sibling-combinator.js
@@ -5,28 +5,28 @@ var expect = require('chai').expect
 
 describe('query:', function() {
 
-  it('should query with next sibling combinator (p em + text)',
+  it('should query with following sibling combinator (p em ~ text)',
     function(done) {
-      var selector = 'p em + text'
+      var selector = 'p em ~ text'
         , result = query('Paragraph *emph*, **strong** and `code`', selector);
-      expect(result.length).to.eql(1);
 
-      expect(result[0].type).to.eql(Node.TEXT);
-      expect(result[0].literal).to.eql(', ');
-      done();
-    }
-  );
-
-  it('should query with next sibling combinator (p * + text)',
-    function(done) {
-      var selector = 'p * + text'
-        , result = query('Paragraph *emph*, **strong** and `code`', selector);
       expect(result.length).to.eql(2);
-
       expect(result[0].type).to.eql(Node.TEXT);
       expect(result[0].literal).to.eql(', ');
       expect(result[1].type).to.eql(Node.TEXT);
       expect(result[1].literal).to.eql(' and ');
+      done();
+    }
+  );
+
+  it('should query with following sibling combinator (p text ~ code)',
+    function(done) {
+      var selector = 'p text ~ code'
+        , result = query('Paragraph *emph*, **strong** and `code`', selector);
+
+      expect(result.length).to.eql(1);
+      expect(result[0].type).to.eql(Node.CODE);
+      expect(result[0].literal).to.eql('code');
       done();
     }
   );
