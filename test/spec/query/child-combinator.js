ea9f4abd21262e395fa35652f8cc09576cd943fa
muji <freeformsystems@gmail.com>
2016-04-23 16:40:39 +0800

    Improve handling of child combinator.

--- test/spec/query/child-combinator.js
+++ test/spec/query/child-combinator.js
@@ -1,11 +1,12 @@
 var expect = require('chai').expect
   , ast = require('mkast')
   , Node = ast.Node
+  , compile = require('../../../index').compile
   , query = require('../../../index').query;
 
 describe('query:', function() {
 
-  it('should match with child combinator (p > text) - mixed', function(done) {
+  it('should match with child combinator (p > text)', function(done) {
     var selector = 'p > text'
       , result = query('Paragraph *emph*, **strong** and `code`', selector);
 
@@ -16,12 +17,25 @@ describe('query:', function() {
     done();
   });
 
-  it('should match with child combinator (p > em) - mixed', function(done) {
+  it('should match with child combinator (p > em)', function(done) {
     var selector = 'p > em'
       , result = query('Paragraph *emph*, **strong** and `code`', selector);
 
     expect(result.length).to.eql(1);
     expect(result[0].type).to.eql(Node.EMPH);
+    done();
+  });
+
+  it('should match with child combinator (> p)', function(done) {
+    var selector = '> p'
+      , result = query('Paragraph *emph*, **strong** and `code`', selector);
+
+    console.error(compile(selector))
+
+    expect(result.length).to.eql(1);
+    expect(result[0].type).to.eql(Node.PARAGRAPH);
+    //expect(result[1].literal).to.eql(', ');
+    //expect(result[2].literal).to.eql(' and ');
     done();
   });
 
