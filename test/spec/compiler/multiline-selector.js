fff1414394578bfc2a2354b3a1e3827116236746
muji <freeformsystems@gmail.com>
2016-04-23 09:30:55 +0800

    Ensure multiple queries go on root selectors list.

--- test/spec/compiler/multiline-selector.js
+++ test/spec/compiler/multiline-selector.js
@@ -6,6 +6,26 @@ var expect = require('chai').expect
 describe('compiler:', function() {
   
   it('should compile selector spanning multiple lines', function(done) {
+    var selector = 'ul,\n\nol li'
+      , result = compile(selector);
+
+    //console.error(result);
+
+    expect(result).to.be.an('object');
+    expect(result.selectors).to.be.an('array')
+      .to.have.length(2);
+    expect(result.selectors[0].tag).to.eql('ul');
+    expect(result.selectors[0].type).to.eql(Node.LIST);
+    expect(result.selectors[1].tag).to.eql('ol');
+    expect(result.selectors[1].type).to.eql(Node.LIST);
+
+    // nested child selector
+    expect(result.selectors[1].selector.tag).to.eql('li');
+    done();
+  });
+
+  it('should compile selector spanning multiple lines', function(done) {
+    // NOTE: the newline in the descendant selector part
     var selector = 'ul,\n\nol\nli'
       , result = compile(selector);
 
