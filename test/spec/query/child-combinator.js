939aa23718a10d8443242d63337dfb75146911d2
muji <freeformsystems@gmail.com>
2016-04-23 16:42:00 +0800

    Improve test spec.

--- test/spec/query/child-combinator.js
+++ test/spec/query/child-combinator.js
@@ -1,7 +1,6 @@
 var expect = require('chai').expect
   , ast = require('mkast')
   , Node = ast.Node
-  , compile = require('../../../index').compile
   , query = require('../../../index').query;
 
 describe('query:', function() {
@@ -28,14 +27,10 @@ describe('query:', function() {
 
   it('should match with child combinator (> p)', function(done) {
     var selector = '> p'
-      , result = query('Paragraph *emph*, **strong** and `code`', selector);
-
-    console.error(compile(selector))
+      , result = query('Paragraph\n\n* List item\n\n', selector);
 
     expect(result.length).to.eql(1);
     expect(result[0].type).to.eql(Node.PARAGRAPH);
-    //expect(result[1].literal).to.eql(', ');
-    //expect(result[2].literal).to.eql(' and ');
     done();
   });
 
